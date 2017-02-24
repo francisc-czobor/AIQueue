0808794c7cb064303314fd4efb1a0507147bfcbc
Unknown <radu_mocanu2000@yahoo.com>
2017-02-24 22:50:51 +0200

    Seara cea Mare 24 februarie

--- C++ 11/RandomBot.cpp
+++ C++ 11/RandomBot.cpp
@@ -9,6 +9,99 @@
 #include "hlt.hpp"
 #include "networking.hpp"
 
+/* 
+#define STILL 0
+#define NORTH 1
+#define EAST 2
+#define SOUTH 3
+#define WEST 4 
+*/
+
+unsigned char out_direction(unsigned short b, unsigned short a, hlt::GameMap m) {
+
+    unsigned char owner = (m.getSite({ b, a })).owner, d = 3;
+    short r;
+    
+    for(unsigned short i = 0; i < m.width || i < m.height; i++) {
+
+        if((m.getSite({ b, (unsigned short)((a + i) % m.height) })).owner != owner)
+            return 3;
+
+        r = a - i;
+        if(r < 0)
+            r += 30;  
+
+        if((m.getSite({ b, (unsigned short)r })).owner != owner)
+            return 1;
+
+        if((m.getSite({ (unsigned short)((b + i) % m.width), a })).owner != owner)
+            return 2;
+
+         r = b - i;
+        if(r < 0)
+            r += 30;
+
+        if((m.getSite({ (unsigned short)r, a })).owner != owner)
+            return 4;
+    }
+
+    return 0;
+}
+
+unsigned char get_direction(unsigned short b, unsigned short a, hlt::GameMap m) {
+
+    int i = 0;
+    unsigned char owner = (m.getSite({ b, a })).owner, strength = (m.getSite({ b, a })).strength;
+    hlt::Site s = m.getSite({ b, (unsigned short)((a + 1) % m.height)});
+
+    if(s.owner == owner) {
+        i++;
+    } else if(s.strength < strength) {
+        return 3;
+    }
+
+    if(a)
+        s = m.getSite({ b, (unsigned short)((a - 1) % m.height)});
+    else 
+        s  = m.getSite({ b, 29 });
+
+    if(s.owner == owner) {
+        i++;
+    } else if(s.strength < strength) {
+        return 1;
+    }
+
+    s = m.getSite({(unsigned short)((b + 1) % m.width), a});
+
+
+    if(s.owner == owner) {
+        i++;
+    } else if(s.strength < strength) {
+        return 2;
+    }
+
+    if(b)
+        s = m.getSite({(unsigned short)((b - 1) % m.width), a});
+    else 
+        s = m.getSite({29, a});
+
+    if(s.owner == owner) {
+        i++;
+    } else if(s.strength < strength) {
+        return 4;
+    }
+
+    unsigned char prod = (m.getSite({ b, a })).production;
+    
+    if(prod && strength/prod <= 5)
+        return 0;
+
+    if(i == 4)
+        return out_direction(b, a, m);
+
+    return 0;
+}
+
 int main() {
     srand(time(NULL));
 
@@ -16,19 +109,31 @@ int main() {
 
     unsigned char myID;
     hlt::GameMap presentMap;
+    hlt::Site s;
     getInit(myID, presentMap);
-    sendInit("RandomC++Bot");
+
+
+
+    sendInit("MyC++Bot2");
+
+    
 
     std::set<hlt::Move> moves;
     while(true) {
         moves.clear();
-
         getFrame(presentMap);
 
         for(unsigned short a = 0; a < presentMap.height; a++) {
             for(unsigned short b = 0; b < presentMap.width; b++) {
-                if (presentMap.getSite({ b, a }).owner == myID) {
-                    moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
+                if((s = presentMap.getSite({ b, a })).owner == myID) {
+                    
+                    if(s.strength == 0) {
+                        moves.insert({ { b, a }, 0 });
+
+                    } else {
+
+                        moves.insert({ { b, a }, get_direction(b, a, presentMap) });
+                    }
                 }
             }
         }
